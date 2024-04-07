aee680af151411f13062b9073d028f82754910dc
Sushanth <sushanthmachavolu@gmail.com>
2024-04-07 20:21:12 +0530

    commit9

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener("scroll", () => {
   //Scroll Progress
   document.getElementById("scrollProgress1").style.width = scrollPercent + "%";
   let noScrollPercent = 100 - scrollPercent;
+  noScrollPercent = scrollPercent > 100 ? 0 : noScrollPercent; //Understand This
   document.getElementById("scrollProgress2").style.width =
     noScrollPercent + "%";
 
@@ -20,12 +21,12 @@ window.addEventListener("scroll", () => {
 });
 
 //3d Button at Submit and reset buttons in contact form
-button = (id) => {
-  if (id == "button1") {
-    document.getElementById("button1").style.boxShadow = "0rem 0.15rem #2b7a78";
-    document.getElementById("button1").style.transform = "translateY(0.15rem)";
-  } else if (id == "button2") {
-    document.getElementById("button2").style.boxShadow = "0rem 0.15rem #2b7a78";
-    document.getElementById("button2").style.transform = "translateY(0.15rem)";
-  }
-};
+// button = (id) => {
+//   if (id == "button1") {
+//     document.getElementById("button1").style.boxShadow = "0rem 0.15rem #2b7a78";
+//     document.getElementById("button1").style.transform = "translateY(0.15rem)";
+//   } else if (id == "button2") {
+//     document.getElementById("button2").style.boxShadow = "0rem 0.15rem #2b7a78";
+//     document.getElementById("button2").style.transform = "translateY(0.15rem)";
+//   }
+// };
